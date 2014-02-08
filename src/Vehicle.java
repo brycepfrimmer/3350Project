75b9b55eada4633adab77942e29b06bee35cd4ee
delroyhiebert <delroy.hiebert@gmail.com>
2014-02-07 19:58:57 -0600

    Error on previous commit, this should be the correct vehicle file now

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -5,7 +5,7 @@
  *         		      Bryce Pfrimmer
  *                    Cody Edwards
  *                    Darwin  Froese
- *                    Delroy Heibert
+ *                    Delroy Hiebert
  *                    Zac Medeiros
  *                  Copyright (c) 2014
  ****************************************************
@@ -19,25 +19,29 @@ public class Vehicle
 {
 	
 	//Class variables, important information about each object.
+	/*Mandatory Fields*/
 	private String ID;
 	private String type;
 	private String manufacturer;
 	private String model;
-	private int kmDriven;
-	private int kmLastServiced;
-	
-	private PartsList partsList;
-	
-	private InsurancePolicy insurance;
-	
-	private double fuelEcon;
 	private boolean roadWorthy;
 	private String licensePlate;
-	private boolean operational;	
+	private boolean operational;
+	private InsurancePolicy insurance;
 	
-	//Vehicle constructor
+	/*OptionalFields*/
+	private int kmDriven;
+	private int kmLastServiced;
+	private PartsList partsList;
+	
+	/*Calculated fields*/
+	private double fuelEcon;
+		
+	
+	//******Vehicle constructor******//
 	public Vehicle()
 	{
+		//Initially set everything to null
 		this.ID = null;
 		this.type = null;
 		this.manufacturer = null;
@@ -53,61 +57,87 @@ public class Vehicle
 		
 		//Get information from the user
 		setData();
-	}//End vehicle()
+	}//End vehicle()	
 	
+	public void print()
+	{
+		System.out.println("Vehicle ID number: " + this.getID());
+		System.out.println("\tType: " + this.getType());
+		System.out.println("\tManufacturer: " + this.getManufacturer());
+		System.out.println("\tModel: " + this.getModel());
+		
+		System.out.println("\tCurrent kilometers: " + this.getKmDriven());
+		System.out.println("\tKilometers when the vehicle was last serviced: " + this.getKmLastServiced());
+		
+		this.getInsurance().print();
+		
+		System.out.println("\tCurrent fuel economy: " + this.getFuelEcon());
+		
+		System.out.print("\tVehicle roadworthy: ");
+		if (this.isRoadWorthy()){ 
+			System.out.println("Yes");
+			System.out.println("\tLicense plate: " + this.getLicensePlate());
+		}
+		else{ System.out.println("No"); }
+		
+		System.out.print("\tVehicle operational: ");
+		if (this.isOperational()){ System.out.println("Yes"); }
+		else{ System.out.println("No"); }
+		
+		System.out.println();
+	}//End print()
+	
+	//***set data methods***//
 	private void setData()
 	{
-		//Required method variables
+		/*Required method variables
+		 *for temporary data storage*/
 		int reply;
-		String policyNum;
-		String type;
-		String input;
-		boolean isNumeric;
 		
-		this.ID = JOptionPane.showInputDialog( "Please enter the vehicle ID number" );
-		this.type = JOptionPane.showInputDialog("Enter the vehicle type. Ex car, truck, forklift, etc." );
-		this.manufacturer = JOptionPane.showInputDialog("Please enter the vehicle manufacturer.");
-		this.model = JOptionPane.showInputDialog("Please enter the vehicle model.");
+		this.setID();
+		this.setType();
+		this.setManufacturer();
+		this.setModel();
 		
 		reply = JOptionPane.showConfirmDialog(null, "Do you wish to enter the kilometers on the vehicle?", "Odometer", JOptionPane.YES_NO_OPTION);
         if (reply == JOptionPane.YES_OPTION) {
-        	input = JOptionPane.showInputDialog("Enter the kilometers on the vehicle.");
-        	isNumeric = input.matches("[0-9]+");// 1 or more characters long, numbers only
-        	isNumeric = input.matches("[0-9]*");// 0 or more characters long, numbers only
-        	while (!isNumeric)
-        	{
-        		input = JOptionPane.showInputDialog("Invalid input! Enter the number of kilometers on the vehicle.");
-            	isNumeric = input.matches("[0-9]+");
-            	isNumeric = input.matches("[0-9]*");
-        	}
-        	this.kmDriven = new Integer(input);
-        	
+        	this.setKmDriven();
         }
         
 		reply = JOptionPane.showConfirmDialog(null, "Do you wish to enter the kilometers the vehicle was last serviced at?", "Last serviced", JOptionPane.YES_NO_OPTION);
         if (reply == JOptionPane.YES_OPTION) {
-        	input = JOptionPane.showInputDialog("Enter the kilometers the vehicle was last serviced at");
-        	isNumeric = input.matches("[0-9]+");// 1 or more characters long, numbers only
-        	isNumeric = input.matches("[0-9]*");// 0 or more characters long, numbers only
-        	while (!isNumeric)
-        	{
-        		input = JOptionPane.showInputDialog("Invalid input! Enter the number of kilometers the vehicle was last serviced at.");
-            	isNumeric = input.matches("[0-9]+");
-            	isNumeric = input.matches("[0-9]*");
-        	}
-        	this.kmLastServiced = new Integer(input);
+        	this.setKmLastServiced();
         }
+        
+        this.setRoadWorthy();
+        this.setInsurance();
+        this.setOperational();
         
         reply = JOptionPane.showConfirmDialog(null, "Do you wish to enter the parts list for the vehicle?", "Parts list", JOptionPane.YES_NO_OPTION);
         if (reply == JOptionPane.YES_OPTION) {
-        	//JOptionPane.showInputDialog("Enter the kilometers the vehicle was last serviced at");
-        }
-        
-        policyNum = JOptionPane.showInputDialog("Please enter the vehicle insurance policy number.");
-        type = JOptionPane.showInputDialog("Please enter the type of insurance or a type about the insurance.");
-        insurance = new InsurancePolicy( policyNum, type );
-        
-        reply = JOptionPane.showConfirmDialog(null, "Is the vehicle roadworthy?", "Roadworthiness", JOptionPane.YES_NO_OPTION);
+        	this.setPartsList();
+        }  
+	}//End setData()
+	
+	public void setID() {
+		this.ID = JOptionPane.showInputDialog( "Please enter the vehicle ID number" );
+	}
+
+	public void setType() {
+		this.type = JOptionPane.showInputDialog("Enter the vehicle type. Ex car, truck, forklift, etc." );
+	}
+
+	public void setManufacturer() {
+		this.manufacturer = JOptionPane.showInputDialog("Please enter the vehicle manufacturer.");
+	}
+
+	public void setModel() {
+		this.model = JOptionPane.showInputDialog("Please enter the vehicle model.");
+	}
+
+	public void setRoadWorthy() {
+		int reply;
+		reply = JOptionPane.showConfirmDialog(null, "Is the vehicle roadworthy?", "Roadworthiness", JOptionPane.YES_NO_OPTION);
         if (reply == JOptionPane.YES_OPTION) {
         	this.roadWorthy = true;
         	this.licensePlate = JOptionPane.showInputDialog("Please enter the vehicle license plate number.");
@@ -117,8 +147,15 @@ public class Vehicle
         	this.roadWorthy = false;
         	this.licensePlate = null;
         }
-        
-        reply = JOptionPane.showConfirmDialog(null, "Is the vehicle operational?", "Operational", JOptionPane.YES_NO_OPTION);
+	}
+
+	public void setLicensePlate() {
+		this.licensePlate = JOptionPane.showInputDialog("Please enter the vehicle license plate number.");
+	}
+
+	public void setOperational() {
+		int reply;
+		reply = JOptionPane.showConfirmDialog(null, "Is the vehicle operational?", "Operational", JOptionPane.YES_NO_OPTION);
         if (reply == JOptionPane.YES_OPTION) {
         	this.operational = true;
         }
@@ -126,36 +163,114 @@ public class Vehicle
         {
         	this.operational = false;
         }
-	}//End setData()
+	}
+
+	public void setInsurance() {
+		String policyNum;
+		String type;
+		policyNum = JOptionPane.showInputDialog("Please enter the vehicle insurance policy number.");
+        type = JOptionPane.showInputDialog("Please enter the type of insurance or a type about the insurance.");
+        insurance = new InsurancePolicy( policyNum, type );
+	}
+
+	public void setKmDriven() {
+		String input;
+		boolean isNumeric;
+		input = JOptionPane.showInputDialog("Enter the kilometers on the vehicle.");
+    	isNumeric = input.matches("[0-9]+");// 1 or more characters long, numbers only
+    	isNumeric = input.matches("[0-9]*");// 0 or more characters long, numbers only
+    	while (!isNumeric)
+    	{
+    		input = JOptionPane.showInputDialog("Invalid input! Enter the number of kilometers on the vehicle.");
+        	isNumeric = input.matches("[0-9]+");
+        	isNumeric = input.matches("[0-9]*");
+    	}
+    	this.kmDriven = new Integer(input);
+	}
+
+	public void setKmLastServiced() {
+		String input;
+		boolean isNumeric;
+		input = JOptionPane.showInputDialog("Enter the kilometers the vehicle was last serviced at");
+    	isNumeric = input.matches("[0-9]+");// 1 or more characters long, numbers only
+    	isNumeric = input.matches("[0-9]*");// 0 or more characters long, numbers only
+    	while (!isNumeric)
+    	{
+    		input = JOptionPane.showInputDialog("Invalid input! Enter the number of kilometers the vehicle was last serviced at.");
+        	isNumeric = input.matches("[0-9]+");
+        	isNumeric = input.matches("[0-9]*");
+    	}
+    	this.kmLastServiced = new Integer(input);
+	}
+
+	public void setPartsList() {
+
+		partsList = new PartsList();
+		String input;
+    	input = JOptionPane.showInputDialog("Enter a vehicle part. Enter 'quit' to stop entering parts.");
+    	while ( !input.equals("quit") )
+    	{
+    		partsList.addPart(input);
+    		input = JOptionPane.showInputDialog("Enter a vehicle part. Enter 'quit' to stop entering parts.");
+    	}
+	}
+	//***End of set methods***//
+
+
 	
-	public void print()
-	{
-		System.out.println("Vehicle ID number: " + this.ID);
-		System.out.println("\tType: " + this.type);
-		System.out.println("\tManufacturer: " + this.manufacturer);
-		System.out.println("\tModel: " + this.model);
-		
-		System.out.println("\tCurrent kilometers: " + this.kmDriven);
-		System.out.println("\tKilometers when the vehicle was last serviced: " + this.kmLastServiced);
-		
-		this.insurance.print();
-		
-		System.out.println("\tCurrent fuel economy: " + this.fuelEcon);
-		
-		System.out.print("\tVehicle roadworthy: ");
-		if (this.roadWorthy){ 
-			System.out.println("Yes");
-			System.out.println("\tLicense plate: " + this.licensePlate);
-		}
-		else{ System.out.println("No"); }
-		
-		System.out.print("\tVehicle operational: ");
-		if (this.operational){ System.out.println("Yes"); }
-		else{ System.out.println("No"); }
-		
-		System.out.println();
-	}//End print()
+	//***get data methods***//
+	public String getID() {
+		return ID;
+	}
 	
+	public String getType() {
+		return type;
+	}
+	
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public int getKmDriven() {
+		return kmDriven;
+	}
+
+	public int getKmLastServiced() {
+		return kmLastServiced;
+	}
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+	public InsurancePolicy getInsurance() {
+		return insurance;
+	}
+
+	public PartsList getPartsList() {
+		return partsList;
+	}
+	
+	public boolean isOperational() {
+		return operational;
+	}
+	
+	public boolean isRoadWorthy() {
+		return roadWorthy;
+	}
+	
+	public double getFuelEcon() {
+		return fuelEcon;
+	}
+	//***End of get data methods***
+	
+	
+	//*******************************************************************************************************
+	//*****Internal private classes*****//
 	private class InsurancePolicy
 	{
 		private String policyNum;
@@ -172,7 +287,7 @@ public class Vehicle
 			System.out.println("\tInsurance policy number: " + this.policyNum);
 			System.out.println("\tInsurance type: " + this.type);
 		}
-	}//End InsurancePolicy
+	}//End InsurancePolicy Class
 	
 	private class PartsList
 	{
@@ -180,16 +295,25 @@ public class Vehicle
 		
 		private PartsList()
 		{
-			partsList = null;
+			partsList = new ArrayList<String>();
+		}
+		
+		private void addPart(String part)
+		{
+			partsList.add(part);
 		}
 		
 		private void print()
 		{
-			//Print code here
+			for( String part : partsList)
+			{
+				System.out.println(part);
+			}
 		}
 		
-	}//End PartsList
-		
+	}//End PartsList Class
+
+	//*******************************************************************************************************	
 }//End Vehicle Class
 	
 	
