18fe33d1b1828d71fddca06dae2161e353474b30
Zac Medeiros <zm4201@gmail.com>
2014-02-13 03:10:40 -0600

    compiles program, test compilation not complete

---
+++ Compile.bat
@@ -0,0 +1,16 @@
+REM @echo off
+
+REM delete all .class files out of the bin directory
+if not exist %CD%\bin mkdir bin
+cd bin
+erase /s *.class
+cd ..
+
+
+call SetClassPath
+
+
+REM @echo off
+javac -d bin\ -cp %classpath% src\org\eclipse\wb\swt\*.java src\cmms\*.java
+REM javac -d bin\ -cp %classpath% src\junitTest\*.java
+
