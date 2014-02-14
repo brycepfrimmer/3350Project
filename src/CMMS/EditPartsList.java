a335cce53ef3503653265ea62ad4fb0d26285f73
codyedwards <cody.w.edwards@gmail.com>
2014-02-13 19:37:50 -0600

    fixed bug where add parts would display kms driven

--- src/CMMS/EditPartsList.java
+++ src/CMMS/EditPartsList.java
@@ -4,6 +4,7 @@ package CMMS;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
@@ -21,6 +22,7 @@ public class EditPartsList {
 	private Button btnRemove;
 	private Table partsListTable;
 	private PartsList list;
+	private static TableColumn partCol;
 
 	/**
 	 * Open the window.
@@ -126,6 +128,8 @@ public class EditPartsList {
 		partsListTable.setBounds(10, 10, 568, 416);
 		partsListTable.setHeaderVisible(true);
 		partsListTable.setLinesVisible(true);
+//		partCol = new TableColumn(partsListTable, SWT.BORDER);
+//		partCol.setWidth(partsListTable.getGridLineWidth());
 	}
 	
 	private void updateList() {
